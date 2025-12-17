43c010979328e4d3ce1dde52d11116949731c238
york <g13539857586@163.com>
2025-12-17 10:02:37 +0800

    取消跟踪.vscode/ 和 .gitignore

---
+++ test/thread_test.c
@@ -0,0 +1,27 @@
+#include "commo_config.h"
+#include "pthread.h"
+
+uint32_t THREAD_ID = 1;
+
+
+void *thread1(void *arg)
+{
+    printf("thread1 start\n");
+}
+
+int main(int argc, char const *argv[])
+{
+
+    if (!pthread_create(&THREAD_ID, NULL, thread1, NULL))
+    {
+        printf("create thread1 success\n");
+    }
+    else
+    {
+        printf("create thread1 failed\n");
+    }
+    while (1) {};
+    return 0;
+}
+
+
