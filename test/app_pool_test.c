ac670cd240fd61a6dd3565893555353bc0814fea
york <g13539857586@163.com>
2025-12-17 17:15:00 +0800

    任务池功能添加及测试代码1

---
+++ test/app_pool_test.c
@@ -0,0 +1,23 @@
+#include "app_pool.h"
+
+void task(void *args)
+{
+    printf("任务%d", *(int *)args);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    app_pool_init(10);
+
+    for (size_t i = 0; i < 20; i++)
+    {
+        app_pool_add_task(&i, task);
+    }
+
+    sleep(1000);
+    return 0;
+}
+
+
+
